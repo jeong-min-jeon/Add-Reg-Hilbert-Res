e0b1b12355ab5aa3517f563e04135cb5dd0fbe4b
Jeong Min Jeon <44523471+jeong-min-jeon@users.noreply.github.com>
2018-12-14 17:38:45 +0100

    Kernel-based functional k-NN estimator for table 2

--- Codes for simulation 1/Kernel-based_function_k-nn_table_2.R
+++ Codes for simulation 1/Kernel-based_function_k-nn_table_2.R
@@ -76,7 +76,6 @@ optimal_k_density=function(X,Y,time_vector,nfolds,k_vec)
       {
         error.fold[k]=error.fold[k]+density_norm(Y.test[i,],Y.test.hat[i,],time_vector)
       }
-      if(length(which(folds==k))==1) error.fold[k]=density_norm(Y.test,Y.test.hat,time_vector)
     }
     error.k[j]=sum(error.fold)
   }
